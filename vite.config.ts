596340bb6a1c3d9905708bf1fec846d71c32386c
SayadSindid <SayadSindid@proton.me>
2025-06-03 19:57:02 +0200

    vite config change #3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: "/TrueMidnight/",
+    base: "/trueMidnight/",
     plugins: [    
         tailwindcss(),
     ],
